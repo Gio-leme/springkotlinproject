1126f4934f6479bf37d8408eb7f488696a174fdb
giovani <giovanileme13@gmail.com>
2023-11-06 00:12:49 -0300

    first commit

---
+++ spring-vscode1/src/main/kotlin/com/giovani/springvscode1/SpringVscode1Application.kt
@@ -0,0 +1,11 @@
+package com.giovani.springvscode1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringVscode1Application
+
+fun main(args: Array<String>) {
+	runApplication<SpringVscode1Application>(*args)
+}
---
+++ spring-vscode1/src/test/kotlin/com/giovani/springvscode1/SpringVscode1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.giovani.springvscode1
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringVscode1ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
